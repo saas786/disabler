00e025a01d2bce827b5d3a1c57034c867fbc0e8f
saasfreelancer <syed.sayal.shah@gmail.com>
2026-06-25 13:16:25 +0500

    dev migrate laravel mix to wp-scripts

---
+++ export.js
@@ -0,0 +1,52 @@
+const { copyItems, removeItems } = require( './bin-dev/copy/fileUtils' );
+
+const exportPath = 'disabler';
+
+const files = [
+  { name: 'disabler.php' },
+  { name: 'uninstall.php' },
+  { name: 'LICENSE.md' },
+  { name: 'composer.json' },
+];
+
+// Call the asynchronous function.
+copyItems( exportPath, files )
+  .then( () => {
+    console.log( 'Files copied successfully!' );
+  } )
+  .catch( ( error ) => {
+    console.error( 'An error occurred:', error );
+  } );
+
+const folders = [
+  { name: 'config' },
+  { name: 'inc' },
+  { name: 'public' },
+  //  { name: 'resources/js' }, // Required for WordPress.org theme review.
+  //  { name: 'resources/scss' }, // Required for WordPress.org theme review.
+  { name: 'vendor' },
+];
+
+// Call the asynchronous function.
+copyItems( exportPath, folders )
+  .then( () => {
+    console.log( 'Folders copied successfully!' );
+  } )
+  .catch( ( error ) => {
+    console.error( 'An error occurred:', error );
+  } );
+
+// Delete unnecessary files and folders.
+const deleteFiles = [
+  `${exportPath}/vendor/bin`,
+  `${exportPath}/vendor/composer/installers`,
+];
+
+// Call the asynchronous function.
+removeItems( deleteFiles )
+  .then( () => {
+    console.log( 'Files copied successfully!' );
+  } )
+  .catch( ( error ) => {
+    console.error( 'An error occurred:', error );
+  } );
