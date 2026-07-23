62c37d9a51f57f34654e54216aac97957c58a6ac
saasfreelancer <syed.sayal.shah@gmail.com>
2026-07-23 14:49:37 +0500

    refactor: align with updated Hybrid APIs

--- inc/Facades/Assets.php
+++ inc/Facades/Assets.php
@@ -7,8 +7,12 @@ use Hybrid\Core\Facades\Facade;
 /**
  * @see \Hybrid\Assets\Plugin
  *
- * @method static string assetUrl(string $file)
- * @method static string assetPath(string $file)
+ * @method static string url(string $file)
+ * @method static string path(string $file)
+ * @method static string assetUrl(string $file, bool $inherit)
+ * @method static string assetPath(string $file, bool $inherit)
+ * @method static \Hybrid\Assets\Asset asset(string $file, bool $inherit, string $overrideManifestDirectory = '')
+ * @method static \Hybrid\Assets\Svg svg(string $file, bool $inherit = false)
  */
 class Assets extends Facade {
     /**
